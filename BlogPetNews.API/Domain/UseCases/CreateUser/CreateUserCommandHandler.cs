49923caf755312a825de54537240070f8acb7cd4
Wellington chida <wellingtonchida@protonmail.com>
2023-08-21 22:42:05 -0300

    Add CQRS create and login user

---
+++ BlogPetNews.API/Domain/UseCases/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BlogPetNews.API.Domain.Users;
+using BlogPetNews.API.Infra.Utils;
+using MediatR;
+
+namespace BlogPetNews.API.Domain.UseCases.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, CreateUserCommandResponse>
+    {
+        private readonly IUserRepository _userService;
+        private readonly TokenService _tokenService;
+
+        public CreateUserCommandHandler(IUserRepository userService, TokenService tokenService)
+        {
+            _userService = userService;
+            _tokenService = tokenService;
+        }
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = Create(request.user);
+            if (user is not null)
+            {
+                return new CreateUserCommandResponse { Success = true, User = user };
+            }
+            else
+                return new CreateUserCommandResponse { Success = false, User = null };
+
+
+        }
+
+        private User Create(User user)
+        {
+            var newUser = _userService.Create(user);
+            if (newUser is not null)
+                return newUser;
+            else
+                return null;
+
+
+        }
+    }
+}
---
+++ BlogPetNews.API/Domain/UseCases/CreateUser/CreateUserCommandResponse.cs
@@ -0,0 +1,10 @@
+﻿using BlogPetNews.API.Domain.Users;
+
+namespace BlogPetNews.API.Domain.UseCases.CreateUser
+{
+    public class CreateUserCommandResponse
+    {
+        public User User { get; set; }
+        public bool Success { get; set; }
+    }
+}
