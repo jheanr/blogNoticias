e8474548d521b12dc241d0113818370fbb98ef55
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-10-17 17:13:49 -0300

    :wrench: chore:  Test unit Helpers

---
+++ BlogPetNews.Unit.Tests/Utils/UnitHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace BlogPetNews.Unit.Tests.Utils
+{
+    public class UnitHelper
+    {
+        public static void AssertStatusCodeOk(HttpResponseMessage response)
+        {
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        public static void AssertStatusCodeBadRequest(HttpResponseMessage response)
+        {
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        public static void AssertStatusCodeUnauthorizedd(HttpResponseMessage response)
+        {
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+
+        public static void AssertStatusCodeInternalServerErrorRequest(HttpResponseMessage response)
+        {
+            Assert.Equal(HttpStatusCode.InternalServerError, response.StatusCode);
+        }
+    }
+}
