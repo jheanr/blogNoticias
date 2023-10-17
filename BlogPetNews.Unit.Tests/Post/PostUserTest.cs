23555fc9849d4904c952815907294ea046284a9a
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-10-17 17:14:17 -0300

    :test_tube: test: Adding post tests

--- BlogPetNews.Unit.Tests/Post/PostUserTest.cs
+++ BlogPetNews.Unit.Tests/Post/PostUserTest.cs
@@ -1,11 +1,104 @@
+using BlogPetNews.API.Domain.Users;
+using BlogPetNews.API.Service.ViewModels.News;
+using BlogPetNews.Unit.Tests.Utils;
+using System.Net.Http.Json;
+
 namespace BlogPetNews.Unit.Tests.Post
 {
     public class PostUserTest
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public async Task POST_CreateUserReturnOk()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            var user = new User
+            {
+                Name = "El Gato",
+                Email = "elgato@miau.net",
+                Password = "gato@321"
+            };
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/create", user);
+
+            UnitHelper.AssertStatusCodeOk(result);
+        }
+
+        [Fact]
+        public async Task POST_CreateUserReturnFailure()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            var user = new User
+            {
+                Name = "El Gato",
+                Email = "elgato@miau.net",
+            };
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/create", user);
+
+            UnitHelper.AssertStatusCodeBadRequest(result);
+        }
+
+        [Fact]
+        public async Task POST_LoginSuccess()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            await PetNewsMockData.CreateUser(application, true);
+
+            var user = new User { Email = "elgato@miau.net",Password = "gato@123"};
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/login", user);
+
+            UnitHelper.AssertStatusCodeOk(result);
+
+        }
+
+        [Fact]
+        public async Task POST_LoginUnauthorized()
+        {
+            await using var application = new PetNewsApiApplication();
+            
+            await PetNewsMockData.CreateUser(application, true);
+
+            var user = new User
+            {
+                Email = "admin@admin.com",
+                Password = "SenhaSecreta"
+            };
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/login", user);
+            
+            UnitHelper.AssertStatusCodeUnauthorizedd(result);
+        }
+
+        [Fact]
+        public async Task POST_CreateNewsReturnsNothAuthorized()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            await PetNewsMockData.CreateNews(application,true);
+
+            var newsDto = new CreateNewsDto
+            {
+                Title = "Capriche no telhado",
+                Content = "Namoro bom é quando você deixa os humanos sem conseguir dormir"
+            };
+
+
+            var client = application.CreateClient();
+            var response = await client.PostAsJsonAsync("/news/create", newsDto);
+
+            var news = await client.GetFromJsonAsync<List<News>>("/news");
+
+            UnitHelper.AssertStatusCodeUnauthorizedd(response);
+            Assert.True(news.Count >= 2);
         }
     }
 }
