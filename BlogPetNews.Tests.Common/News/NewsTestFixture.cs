c6b5776b48edd043d5928e1074cd5bb2ec78b346
Lucas Varela <lucaas.varela@gmail.com>
2023-10-17 18:24:17 -0300

    feat: criação do projeto common para centralizar a criação dos fakers

---
+++ BlogPetNews.Tests.Common/News/NewsTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using BlogPetNews.API.Service.ViewModels.News;
+using Bogus;
+
+namespace BlogPetNews.Tests.Common.News
+{
+    public static class NewsTestFixture
+    {
+        public static Faker<API.Domain.News.News> NewsFaker { get; } = new Faker<API.Domain.News.News>()
+            .RuleFor(n => n.Id, f => Guid.NewGuid())
+            .RuleFor(n => n.Title, f => f.Lorem.Word())
+            .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+
+        public static Faker<CreateNewsDto> CreateNewsDtoFaker { get; } = new Faker<CreateNewsDto>()
+            .RuleFor(n => n.Title, f => f.Lorem.Word())
+            .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+
+        public static Faker<ReadNewsDto> ReadNewsDtoFaker { get; } = new Faker<ReadNewsDto>()
+            .RuleFor(n => n.Id, f => Guid.NewGuid())
+            .RuleFor(n => n.Title, f => f.Lorem.Word())
+            .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+    }
+}
