ac65502ba3e29008e304c2bcd2d827cbfdef4cbc
Lucas Varela <lucaas.varela@gmail.com>
2023-10-18 20:51:55 -0300

    feat: adição de todos os testes felizes de news

--- BlogPetNews.Tests.Common/News/NewsTestFixture.cs
+++ BlogPetNews.Tests.Common/News/NewsTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using BlogPetNews.API.Service.ViewModels.News;
+﻿using BlogPetNews.API.Domain.UseCases.UpdateNews;
+using BlogPetNews.API.Service.ViewModels.News;
 using Bogus;
 
 namespace BlogPetNews.Tests.Common.News
@@ -6,9 +7,9 @@ namespace BlogPetNews.Tests.Common.News
     public static class NewsTestFixture
     {
         public static Faker<API.Domain.News.News> NewsFaker { get; } = new Faker<API.Domain.News.News>()
-            .RuleFor(n => n.Id, f => Guid.NewGuid())
-            .RuleFor(n => n.Title, f => f.Lorem.Word())
-            .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+        .RuleFor(n => n.Id, f => Guid.NewGuid())
+        .RuleFor(n => n.Title, f => f.Lorem.Word())
+        .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
 
         public static Faker<CreateNewsDto> CreateNewsDtoFaker { get; } = new Faker<CreateNewsDto>()
             .RuleFor(n => n.Title, f => f.Lorem.Word())
@@ -18,5 +19,13 @@ namespace BlogPetNews.Tests.Common.News
             .RuleFor(n => n.Id, f => Guid.NewGuid())
             .RuleFor(n => n.Title, f => f.Lorem.Word())
             .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+
+        public static Faker<UpdateNewsDto> UpdateNewsDtoFaker { get; } = new Faker<UpdateNewsDto>()
+            .RuleFor(n => n.Title, f => f.Lorem.Word())
+            .RuleFor(n => n.Content, f => f.Lorem.Paragraph());
+
+        public static Faker<UpdateNewsCommand> UpdateNewsCommandFaker { get; } = new Faker<UpdateNewsCommand>()
+            .RuleFor(n => n.Id, f => Guid.NewGuid())
+            .RuleFor(n => n.UpdateNewsDto, f => UpdateNewsDtoFaker.Generate());
     }
 }
--- BlogPetNews.Unit.Tests/Post/PostNewsTest.cs
+++ BlogPetNews.Unit.Tests/Post/PostNewsTest.cs
@@ -43,6 +43,79 @@ namespace BlogPetNews.Unit.Tests.Post
             Assert.NotNull(response);
             Assert.Equal(readNewsDto, response);
         }
+
+        [Fact]
+        public void Should_Update_News_Successfully()
+        {
+            //Arrange
+            var newsId = Guid.NewGuid();
+            var news = NewsTestFixture.NewsFaker.Generate();
+            var newsDto = NewsTestFixture.UpdateNewsDtoFaker.Generate();
+            var newsUpdate = NewsTestFixture.NewsFaker.Generate();
+            var readNewsDto = NewsTestFixture.ReadNewsDtoFaker.Generate();
+
+            _newsRepository.GetById(newsId).Returns(news);
+            _mapper.Map(newsDto, news).Returns(newsUpdate);
+            _newsRepository.Update(newsUpdate).Returns(newsUpdate);
+            _mapper.Map<ReadNewsDto>(newsUpdate).Returns(readNewsDto);
+
+            //Act
+            var response = _newsService.Update(newsId, newsDto);
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.Equal(readNewsDto, response);
+        }
+
+        [Fact]
+        public void Should_Delete_News_Successfully()
+        {
+            //Arrange
+            var newsId = Guid.NewGuid();
+
+            //Act
+            _newsService.Delete(newsId);
+
+            //Assert
+            _newsRepository.Received(1).Delete(newsId);
+        }
+
+        [Fact]
+        public void Should_GetAll_News_Successfully()
+        {
+            // Arrange
+            var newsList = NewsTestFixture.NewsFaker.Generate(3);
+            var readNewsDtos = NewsTestFixture.ReadNewsDtoFaker.Generate(3);
+
+            _newsRepository.GetAll().Returns(newsList);
+            _mapper.Map<IEnumerable<ReadNewsDto>>(newsList).Returns(readNewsDtos);
+
+            // Act
+            var response = _newsService.GetAll();
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(newsList.Count, response.Count());
+            Assert.Equal(readNewsDtos, response);
+        }
+
+        [Fact]
+        public void Should_GetId_News_Successfully()
+        {
+            // Arrange
+            var newsId = Guid.NewGuid();
+            var news = NewsTestFixture.NewsFaker.Generate();
+            var readNewsDto = NewsTestFixture.ReadNewsDtoFaker.Generate();
+
+            _newsRepository.GetById(newsId).Returns(news);
+            _mapper.Map<ReadNewsDto>(news).Returns(readNewsDto);
+
+            // Act
+            var response = _newsService.GetById(newsId);
+
+            // Assert
+            Assert.Equal(readNewsDto, response);
+        }
         #endregion
     }
 }
