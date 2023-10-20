18ba420e19023dd276323612867d3fbee5ba487c
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-10-20 15:59:06 -0300

    :sparkles: feat: Configuração da WebApplicationFactory, Mock e Helpers para os testes de integração

---
+++ BlogPetNews.Integration.Tests/Util/PetNewsApiApplication.cs
@@ -0,0 +1,29 @@
+﻿using BlogPetNews.API.Infra.Contexts;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace BlogPetNews.Integration.Tests.Util
+{
+    public class PetNewsApiApplication : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            var root = new InMemoryDatabaseRoot();
+
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<BlogPetNewsDbContext>))
+
+                services.AddDbContext<BlogPetNewsDbContext>(options =>
+                    options.UseInMemoryDatabase("BlogPetNewsDb", root));
+            });
+
+            return base.CreateHost(builder);
+        }
+
+    }
+}
