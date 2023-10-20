58f4b82bbefeecc70f7819bc632ab889c1294abf
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-10-20 16:07:54 -0300

    :white_check_mark: test: Testes do endpoint de post de usuário

--- BlogPetNews.Integration.Tests/Post/PostUserTest.cs
+++ BlogPetNews.Integration.Tests/Post/PostUserTest.cs
@@ -1,25 +1,82 @@
+using BlogPetNews.API.Domain.News;
+using BlogPetNews.API.Domain.Users;
+using BlogPetNews.API.Service.ViewModels.News;
+using BlogPetNews.Integration.Tests.Util;
+using Microsoft.AspNetCore.Http.Extensions;
+using System.Net.Http.Json;
+
 namespace BlogPetNews.Integration.Tests.Post
 {
     public class PostUserTest
     {
         [Fact]
-        public void Should_Create_NewUser_Successfully()
+        public async Task POST_CreateUserReturnOk()
         {
-            //Arrange
+            await using var application = new PetNewsApiApplication();
 
-            //Act
+            var user = new User
+            {
+                Name = "El Gato",
+                Email = "elgato@miau.net",
+                Password = "gato@321"
+            };
 
-            //Assert
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/create", user);
+
+            IntegrationTestHelpers.AssertStatusCodeOk(result);
         }
 
         [Fact]
-        public void Should_Login_User_Successfully()
+        public async Task POST_CreateUserReturnFailure()
         {
-            //Arrange
+            await using var application = new PetNewsApiApplication();
 
-            //Act
+            var user = new User
+            {
+                Name = "El Gato",
+                Email = "elgato@miau.net",
+            };
 
-            //Assert
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/create", user);
+
+            IntegrationTestHelpers.AssertStatusCodeBadRequest(result);
+        }
+
+        [Fact]
+        public async Task POST_LoginSuccess()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            await PetNewsMockData.CreateUser(application, true);
+
+            var user = new User { Email = "elgato@miau.net", Password = "gato@123" };
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/login", user);
+
+            IntegrationTestHelpers.AssertStatusCodeOk(result);
+
+        }
+
+        [Fact]
+        public async Task POST_LoginUnauthorized()
+        {
+            await using var application = new PetNewsApiApplication();
+
+            await PetNewsMockData.CreateUser(application, true);
+
+            var user = new User
+            {
+                Email = "admin@admin.com",
+                Password = "SenhaSecreta"
+            };
+
+            var client = application.CreateClient();
+            var result = await client.PostAsJsonAsync("/login", user);
+
+            IntegrationTestHelpers.AssertStatusCodeUnauthorized(result);
         }
     }
 }
